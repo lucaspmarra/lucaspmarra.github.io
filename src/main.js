999cd88cc16615cb8d806ee5bf92f6b7d347df6e
Lucas Marra <marralucas1@gmail.com>
2024-10-27 23:10:46 -0300

    add gtag to project and remove base url

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createGtm } from '@gtm-support/vue-gtm';
+import VueGtag from "vue-gtag";
 
 import App from './App.vue'
 import router from './router'
@@ -11,8 +11,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(createGtm({
-  id: 'G-BM2FRYX5HE'
-}))
+app.use(VueGtag, {
+  config: { id: "G-BM2FRYX5HE" }
+})
 
 app.mount('#app')
